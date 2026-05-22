5e035af0508c4de7f427943220b5e0dcc911e13b
mahasan24 <mahasan24@students.oamk.fi>
2026-05-22 18:16:15 +0545

    init

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,55 @@
+import { createDb } from "@lets_work/db";
+import * as schema from "@lets_work/db/schema/auth";
+import { env } from "@lets_work/env/server";
+import { polar, checkout, portal } from "@polar-sh/better-auth";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { polarClient } from "./lib/payments";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [
+      polar({
+        client: polarClient,
+        createCustomerOnSignUp: true,
+        enableCustomerPortal: true,
+        use: [
+          checkout({
+            products: [
+              {
+                productId: "your-product-id",
+                slug: "pro",
+              },
+            ],
+            successUrl: env.POLAR_SUCCESS_URL,
+            authenticatedUsersOnly: true,
+          }),
+          portal(),
+        ],
+      }),
+    ],
+  });
+}
+
+export const auth = createAuth();
